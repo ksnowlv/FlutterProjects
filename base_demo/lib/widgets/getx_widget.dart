7ec0e35181bf08e7465a76094f64f565418b2844
律威 <lvwei@sogou-inc.com>
2024-03-20 22:24:41 +0800

    添加插件getx及示例

---
+++ base_demo/lib/widgets/getx_widget.dart
@@ -0,0 +1,60 @@
+import 'package:base_demo/widgets/detail_page.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class GetxWidget extends StatefulWidget {
+  const GetxWidget({Key? key}) : super(key: key);
+
+  @override
+  GetxWidgetState createState() => GetxWidgetState();
+}
+
+class GetxWidgetState extends State<GetxWidget> {
+  final CounterController counterController = Get.put(CounterController());
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Obx(() => Text('点击了${counterController.counter.value}次')),
+        const SizedBox(
+          height: 20,
+        ),
+        ElevatedButton(
+            onPressed: () {
+              counterController.increment();
+            },
+            child: const Text('+')),
+        const SizedBox(
+          height: 20,
+        ),
+        ElevatedButton(
+            onPressed: () {
+              counterController.decrement();
+            },
+            child: const Text('+')
+            ),
+
+            ElevatedButton(onPressed: () {
+
+              Get.to(() => const DetailPage(data: 'getx widget'), arguments: '参数传递');
+
+            }, child: const Text('跳转详情页面'))
+      ],
+    );
+  }
+}
+
+// 控制器类，用于管理计数器状态
+class CounterController extends GetxController {
+  var counter = 0.obs; // 通过obs将变量变为响应式变量
+
+  void increment() {
+    counter.value++; // 使用.value来访问obs变量的值
+  }
+
+  void decrement() {
+    counter.value--; // 使用.value来访问obs变量的值
+  }
+}
