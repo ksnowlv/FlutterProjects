51155b2248e780ce36a32ca26426f97fa635a228
律威 <lvwei@sogou-inc.com>
2024-04-30 17:17:24 +0800

    添加文件目录管理

--- base_demo/lib/common/file_mgr.dart
+++ base_demo/lib/common/file_mgr.dart
@@ -4,13 +4,31 @@ class FileMgr {
   FileMgr._privateConstructor();
 
   static final FileMgr instance = FileMgr._privateConstructor();
-
+  //文档目录
   String? _documentsDirectory;
+  //临时目录
+  String? _temporaryDirectory;
+  //
+  String? _cacheDirectory;
 
+  //获取文档目录
   Future<String> getDocumentsDirectory() async {
-   
-    _documentsDirectory??=(await getApplicationDocumentsDirectory()).path;
-    
+    _documentsDirectory ??= (await getApplicationDocumentsDirectory()).path;
+
     return _documentsDirectory!;
+  }
+
+//获取临时目录
+  /// - `NSCachesDirectory` on iOS and macOS.
+  /// - `Context.getCacheDir` on Android.
+  Future<String> getTemporaryDirectoryPath() async {
+    _temporaryDirectory ??= (await getTemporaryDirectory()).path;
+    return _temporaryDirectory!;
+  }
+
+//获取缓存目录
+  Future<String> getApplicationSupportDirectoryPath() async {
+    _cacheDirectory ??= (await getApplicationSupportDirectory()).path;
+    return _cacheDirectory!;
   }
 }
