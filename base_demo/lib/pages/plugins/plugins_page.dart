1b16ae53683d9359efb59846e67e23583193aa92
律威 <lvwei@sogou-inc.com>
2024-04-30 17:43:11 +0800

    添加插件模块关于flutter_bloc路由方式支持示例

--- base_demo/lib/pages/plugins/plugins_page.dart
+++ base_demo/lib/pages/plugins/plugins_page.dart
@@ -1,8 +1,11 @@
 import 'package:base_demo/pages/plugins/audio_players_page.dart';
+import 'package:base_demo/pages/plugins/custom_popup_menu_page.dart';
+import 'package:base_demo/pages/plugins/flutter_cache_manager_page.dart';
 import 'package:base_demo/pages/plugins/image_compress_page.dart';
 import 'package:base_demo/pages/plugins/round_checkbox_page.dart';
 import 'package:base_demo/pages/plugins/video_player_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 enum PluginType {
   videoPlayer, //https://pub-web.flutter-io.cn/packages/video_player
@@ -12,7 +15,9 @@ enum PluginType {
   flutterAnimate, //https://pub-web.flutter-io.cn/packages/flutter_animate
   slidingUpPanel, //https://pub-web.flutter-io.cn/packages/sliding_up_panel
   localAuth, //https://pub-web.flutter-io.cn/packages/local_auth
-  roundcheckbox, //https://pub-web.flutter-io.cn/packages/roundcheckbox
+  roundcheckbox, //https://pub-web.flutter-io.cn/packages/roundcheckbox, 
+  custom_pop_up_menu,//https://pub-web.flutter-io.cn/packages/custom_pop_up_menu
+  flutter_cache_manager,//https://pub-web.flutter-io.cn/packages/flutter_cache_manager
 }
 
 class PluginsPage extends StatefulWidget {
@@ -32,36 +37,32 @@ class _PluginsPageState extends State<PluginsPage> {
     PluginType.flutterAnimate: 'flutter动画组件',
     PluginType.slidingUpPanel: '滑动面板组件',
     PluginType.localAuth: '本地生物识别和设备密码验证组件',
-    PluginType.roundcheckbox: "roundcheckbox"
-    // PluginTypePage.fileWidget: '文件组件',
-    // HomePageRouteType.netWidget: '网络组件',
-    // HomePageRouteType.extensionWidget: '包和插件组件',
+    PluginType.roundcheckbox: "roundcheckbox", 
+    PluginType.custom_pop_up_menu: "自定义弹出菜单组件",
+    PluginType.flutter_cache_manager: "flutter_cache_manager组件",
   };
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: ListView.builder(
-        itemExtent: 60,
-        itemCount: PluginType.values.length,
-        itemBuilder: (BuildContext context, int index) {
-          final pageType = PluginType.values[index];
-          return   Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ElevatedButton(
-                      onPressed: () => _onShowPage(context, pageType),
-                      child: Text(
-                        _getPageTypeText(pageType),
-                      )),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                ],
-              );
-        },
-      ),
-    );
+    return BlocBuilder<PluginsPageCubit, String>(builder: (context, state) {
+
+      if(state == 'videoplayer') {
+        return VideoPlayerPage(pageTitle: "",);
+      } else if (state == 'imagecompress') {
+        return ImageCompressPage();
+      } else if (state == 'audioplayers') {
+        return AudioPlayersPage();
+      } else if (state == 'roundcheckbox') {
+        return RoundCheckboxPage();
+      } else if (state == 'custom_pop_up_menu') {
+        return CustomPopupMenuPage();
+      } else if (state == 'flutter_cache_manager') {
+        return const FlutterCacheManagerPage();
+      }
+
+      return _buildPluginsPages();
+  
+    } );
   }
 
   void _onShowPage(BuildContext context, PluginType pluginType) async {
@@ -101,9 +102,51 @@ class _PluginsPageState extends State<PluginsPage> {
         return const Text('5');
       case PluginType.roundcheckbox:
         return const RoundCheckboxPage();
+      case PluginType.custom_pop_up_menu:
+        return const CustomPopupMenuPage();  
+     case PluginType.flutter_cache_manager:
+        return const FlutterCacheManagerPage();       
 
       default:
         return Text('没有该模块:$pluginType');
     }
   }
+
+  Scaffold _buildPluginsPages() {
+  return  Scaffold(
+      body: ListView.builder(
+        itemExtent: 60,
+        itemCount: PluginType.values.length,
+        itemBuilder: (BuildContext context, int index) {
+          final pageType = PluginType.values[index];
+          return   Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                      onPressed: () => _onShowPage(context, pageType),
+                      child: Text(
+                        _getPageTypeText(pageType),
+                      )),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                ],
+              );
+        },
+      ),
+    );
+  }
+
+  //并使用BlocBuilder来动态显示页面
+
+}
+
+
+// 首页模块的Cubit
+class PluginsPageCubit extends Cubit<String> {
+ PluginsPageCubit() :super('details');
+
+ void navigateTo(String page) {
+   emit(page);
+ }
 }
