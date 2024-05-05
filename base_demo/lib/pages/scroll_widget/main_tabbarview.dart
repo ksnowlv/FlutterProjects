2ddb747b1d647705a926c9b398926832038e6133
ksnowlv <lvwei@sogou-inc.com>
2024-05-05 20:10:22 +0800

    更新顶部tab页切换页面的tab区域选中效果代码

--- base_demo/lib/pages/scroll_widget/main_tabbarview.dart
+++ base_demo/lib/pages/scroll_widget/main_tabbarview.dart
@@ -1,30 +1,42 @@
 import 'package:flutter/material.dart';
 
 class MainTabBarView extends StatelessWidget {
+  static final List<String> tabTitles = ['首页','通讯录','发现','我的'];
+
   const MainTabBarView({super.key});
 
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-        length: 4,
+        length: tabTitles.length,
         child: Scaffold(
           appBar: AppBar(
-             title: const Text('flutter app'),
-            bottom: const TabBar(
-              tabs: [
-                Tab(
-                  text: '首页',
-                ),
-                Tab(
-                  text: '通讯录',
-                ),
-                Tab(
-                  text: '发现',
-                ),
-                Tab(
-                  text: '我的',
-                )
-              ],
+            title: const Text('flutter app'),
+            bottom: TabBar(
+              tabs: tabTitles.map((title) => Tab(text: title)).toList(),
+              indicatorColor: Colors.blue,
+              labelColor: Colors.blue,//Colors.white
+              unselectedLabelColor: Colors.grey,
+              labelStyle: const TextStyle(
+                fontSize: 22,
+              ),
+              unselectedLabelStyle: const TextStyle(
+                fontSize: 20,
+              ),
+              indicatorSize: TabBarIndicatorSize.tab,
+              //若打开下述指示器效果，labelColor由Colors.blue更换为Colors.white
+              // indicator: BoxDecoration(
+              //   color:Colors.blue,// 背景颜色为蓝色
+              //   border: Border.all(color: Colors.blue, width: 2),
+              //   borderRadius: BorderRadius.circular(10),
+              //   boxShadow: const [
+              //     BoxShadow(
+              //       color: Colors.blue,
+              //       offset: Offset(3, 3),
+              //       blurRadius: 5,
+              //     ),
+              //   ], // 阴影效果
+              // ),
             ),
           ),
           body: const TabBarView(
@@ -60,7 +72,6 @@ class ContactsTab extends StatelessWidget {
     );
   }
 }
-
 class DiscoverTab extends StatelessWidget {
   const DiscoverTab({super.key});
 
