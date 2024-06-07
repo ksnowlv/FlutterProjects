e90da049fb40a1139cf1db7ff58106b31399ef86
ksnowlv <lvwei@sogou-inc.com>
2024-06-07 15:35:27 +0800

    更新消息散列示例

--- base_demo/lib/pages/plugins/crypto_page.dart
+++ base_demo/lib/pages/plugins/crypto_page.dart
@@ -19,8 +19,12 @@ enum CryptoType {
   sha512_224,
   sha512_256,
   md5,
-  hmac256, 
-  hmac512, 
+  hmac224,
+  hmac256,
+  hmac384,
+  hmac512,
+  hmac512_224,
+  hmac512_256,
   hmacmd5, // (i.e. HMAC-MD5, HMAC-SHA1, HMAC-SHA256)
 }
 
@@ -34,87 +38,40 @@ class CryptoPage extends StatefulWidget {
 }
 
 class _CryptoPageState extends State<CryptoPage> {
+  static final hmacKey = utf8.encode('test_p@assw0rd1_hamc');
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            ElevatedButton(
-                onPressed: () => _hashString('hello world', CryptoType.sha1),
-                child: const Text('sha1')),
-            const SizedBox(
-              height: 20,
-            ),
-            ElevatedButton(
-                onPressed: () => _hashString('hello world', CryptoType.sha224),
-                child: const Text('sha224')),
-            const SizedBox(
-              height: 20,
-            ),
-            ElevatedButton(
-                onPressed: () => _hashString('hello world', CryptoType.sha256),
-                child: const Text('sha256')),
-            const SizedBox(
-              height: 20,
-            ),
-            ElevatedButton(
-                onPressed: () => _hashString('hello world', CryptoType.sha384),
-                child: const Text('sha384')),
-            const SizedBox(
-              height: 20,
-            ),
-            ElevatedButton(
-                onPressed: () => _hashString('hello world', CryptoType.sha512),
-                child: const Text('sha512')),
-            const SizedBox(
-              height: 20,
-            ),
-            ElevatedButton(
-                onPressed: () =>
-                    _hashString('hello world', CryptoType.sha512_224),
-                child: const Text('sha512_224')),
-            const SizedBox(
-              height: 20,
-            ),
-            ElevatedButton(
-                onPressed: () =>
-                    _hashString('hello world', CryptoType.sha512_256),
-                child: const Text('sha512_256')),
-            const SizedBox(
-              height: 20,
-            ),
-            ElevatedButton(
-                onPressed: () => _hashString('hello world', CryptoType.md5),
-                child: const Text('md5')),
-            const SizedBox(
-              height: 20,
-            ),
-            ElevatedButton(
-                onPressed: () => _hashString('hello world', CryptoType.hmac256),
-                child: const Text('hmac256')),
-            const SizedBox(
-              height: 20,
-            ),
-                  ElevatedButton(
-                onPressed: () => _hashString('hello world', CryptoType.hmac512),
-                child: const Text('hmac512')),
-            const SizedBox(
-              height: 20,
-            ),
-                    ElevatedButton(
-                onPressed: () => _hashString('hello world', CryptoType.hmacmd5),
-                child: const Text('hmacmd5')),
-            const SizedBox(
-              height: 20,
-            ),
-          ],
+        appBar: AppBar(
+          title: Text(widget.title),
         ),
-      ),
-    );
+        body: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              children: _buildButtons(),
+            ),
+          ),
+        ));
+  }
+
+  List<Widget> _buildButtons() {
+    List<CryptoType> types = CryptoType.values;
+    return types.map((type) {
+      return Padding(
+        padding: const EdgeInsets.symmetric(
+          vertical: 10,
+        ),
+        child: ElevatedButton(
+          onPressed: () => _hashString('hello world', type),
+          child: Text(_getButtonLabel(type)),
+        ),
+      );
+    }).toList();
+  }
+
+  String _getButtonLabel(CryptoType type) {
+    return type.toString().split('.').last; // 获取枚举项的字符串形式
   }
 
   String _hashString(String text, CryptoType type) {
@@ -122,66 +79,79 @@ class _CryptoPageState extends State<CryptoPage> {
       return '';
     }
     var bytes = utf8.encode(text); // data being hashed
-    var res = '';
+
+    var convert;
 
     switch (type) {
       case CryptoType.sha1:
-        res = sha1.convert(bytes).toString();
+        convert = sha1;
         break;
       case CryptoType.sha224:
-        res = sha224.convert(bytes).toString();
+        convert = sha224;
         break;
       case CryptoType.sha256:
-        res = sha256.convert(bytes).toString();
+        convert = sha256;
         break;
 
       case CryptoType.sha384:
-        res = sha384.convert(bytes).toString();
+        convert = sha384;
         break;
 
       case CryptoType.sha512:
-        res = sha512.convert(bytes).toString();
+        convert = sha512;
         break;
 
       case CryptoType.sha512_224:
-        res = sha512224.convert(bytes).toString();
+        convert = sha512224;
         break;
 
       case CryptoType.sha512_256:
-        res = sha512256.convert(bytes).toString();
+        convert = sha512256;
         break;
 
       case CryptoType.md5:
-        res = md5.convert(bytes).toString();
+        convert = md5;
+        break;
+
+      case CryptoType.hmac224:
+        convert = Hmac(sha224, hmacKey);
+
         break;
 
       case CryptoType.hmac256:
-        var key = utf8.encode('p@ssw0rd');
+        convert = Hmac(sha256, hmacKey);
 
-        var hmacSha256 = Hmac(sha256, key);
-        res = hmacSha256.convert(bytes).toString();
+        break;
+
+      case CryptoType.hmac384:
+        convert = Hmac(sha384, hmacKey);
 
         break;
 
       case CryptoType.hmac512:
-        var key = utf8.encode('p@ssw0rd');
+        convert = Hmac(sha512, hmacKey);
 
-        var hmacSha512 = Hmac(sha512, key);
-        res = hmacSha512.convert(bytes).toString();
+        break;
 
-        break;  
+      case CryptoType.hmac512_224:
+        convert = Hmac(sha512224, hmacKey);
+
+        break;
+
+      case CryptoType.hmac512_256:
+        convert = Hmac(sha512256, hmacKey);
+
+        break;
 
       case CryptoType.hmacmd5:
-        var key = utf8.encode('p@ssw0rd');
+        convert = Hmac(md5, hmacKey);
 
-        var hmacMD5 = Hmac(md5, key);
-        res = hmacMD5.convert(bytes).toString();
-
-        break; 
+        break;
 
       default:
     }
 
+    var res = convert != null ? convert.convert(bytes).toString() : '';
     debugPrint('_hashString:$res');
 
     return res;
